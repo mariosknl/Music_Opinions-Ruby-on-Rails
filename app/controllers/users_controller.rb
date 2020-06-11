61a35fcb622b025cf06a110a3676f1672615602d
mariosknl <mariosknl@outlook.com>
2020-06-11 09:40:31 +0300

    Session Controller && Sign in/out/up added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,70 +7,51 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @users = User.all
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit; end
-
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.create(user_params)
+    if @user.save
+      @user = User.find_by(username: @user.username)
+      signed_in? ? redirect_to(new_user_path) : create_session_and_redirect(@user)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
-    params.fetch(:user, {})
+    params.require(:user).permit(:username, :fullname)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,14 @@
 module SessionsHelper
+  def signed_in?
+    session[:current_user_id].nil? ? false : true
+  end
+
   def current_user
     User.find(current_user_id) if signed_in?
   end
 
   def current_user_id
     session[:current_user_id]
-  end
-
-  def signed_in?
-    session[:current_user_id].nil? ? false : true
   end
 
   def create_session(user_id)
